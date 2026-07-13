40554e17a3ecbc9b55f8c99a6c4fa9cfb800327a
fzhk656p9k-prog <fzhk656p9k@privaterelay.appleid.com>
2026-07-13 22:26:42 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rhinocase-v08';
+const C='rhinocase-v09';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C)
   .then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png']))
   .then(()=>self.skipWaiting()));});
